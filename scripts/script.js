63a32f9732af2d84b59554b1f9fc7dfbdbbb54a7
DRdeRomas <145459284+DRdeRomas@users.noreply.github.com>
2025-01-19 17:42:27 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,47 +1,66 @@
-// JavaScript Document
-// console.log("hi");
+console.log("Welkom bij ASKV");
+// HAMBURGER MENU
+// var openButton = document.querySelector("header > button");
 
-// const searchMenu = document.querySelector("header > ul");
-// const openSearch = document.querySelector("nav svg");
-// const menuSluitButton = document.querySelector("button.close-menu");
+// openButton.onclick = openMenu;
 
-// openSearch.onclick = toggleSearchMenu;
-// menuSluitButton.onclick -
-
- 
-// function toggleSearchMenu() {
-//   if (searchMenu.classList.contains("inBeeld")) {
-//     searchMenu.classList.remove("inBeeld");
-//   }
-//   else {
-//     searchMenu.classList.add("inBeeld");
-//   }
+// function openMenu() {
+//     var deNav = document.querySelector("nav");
+//     deNav.classList.add("toonMenu");
 // }
 
-// // stap 1 - zoek sluiten button op
-// const menuSluitButton= document.querySelector("nav button");
- 
-// // stap 2 - laat die button luisteren naar kliks
-// menuSluitButton.onclick = sluitMenu;
- 
-// // stap 3 - in de functie verwijder de class van de nav
+// var sluitButton = document.querySelector("nav button");
+// sluitButton.onclick = sluitMenu;
+
 // function sluitMenu(){
-//    deNav.classList.remove("toonMenu");
+//     var deNav = document.querySelector("nav");
+//     deNav.classList.remove("toonMenu");
 // }
 
+// // HAMBURGER MENU
+// var openButton = document.querySelector("header > button");
 
-// Het was mij niet gelukt om een "gecombineerde animatie" te maken met CSS en JS.
-// Deze code heb ik met behulp van ChatGPT gemaakt.
-document.querySelectorAll("details").forEach((detail) => {
-    detail.addEventListener("toggle", function () {
-        if (this.open) {
-            document.querySelectorAll("details").forEach((otherDetail) => {
-                if (otherDetail !== this) {
-                    otherDetail.open = false;
-                }
-            });
-        }
-    });
+// openButton.onclick = openMenu;
+
+// function openMenu() {
+//     var deNav = document.querySelector("nav");
+//     deNav.classList.add("toonMenu");
+// }
+
+// var sluitButton = document.querySelector("nav button");
+// sluitButton.onclick = sluitMenu;
+
+// function sluitMenu(){
+//     var deNav = document.querySelector("nav");
+//     deNav.classList.remove("toonMenu");
+// }
+// Hamburger menu (CodePen opdracht)
+document.addEventListener('DOMContentLoaded', function() {
+    // Hamburger menu openen
+    var openButton = document.querySelector("header > button");
+
+    if (openButton) {
+        openButton.onclick = openMenu;
+    }
+
+    function openMenu() {
+        var deNav = document.querySelector("nav");
+        deNav.classList.add("toonMenu");
+        console.log("Menu is open");
+    }
+
+    // Hamburger menu sluiten
+    var sluitButton = document.querySelector("nav button");
+
+    if (sluitButton) {
+        sluitButton.onclick = sluitMenu;
+    }
+
+    function sluitMenu() {
+        var deNav = document.querySelector("nav");
+        deNav.classList.remove("toonMenu");
+        console.log("Menu is dicht");
+    }
 });
 
 // Hetzelfe gold voor het verbergen van de tekst en input //
@@ -49,57 +68,41 @@ document.addEventListener('DOMContentLoaded', function () {
     const andersRadio = document.querySelector('input[name="donatie_bedrag"][value="ander"]');
     const otherFields = document.querySelectorAll('#aanpassing_bedrag, main section:nth-of-type(1) p:last-of-type');
 
-    // Function to toggle visibility of "other" fields based on the selected radio button
-    function toggleOtherFields() {
-        if (andersRadio.checked) {
-            otherFields.forEach(field => field.style.display = 'block');
-        } else {
-            otherFields.forEach(field => field.style.display = 'none');
+    // Only proceed if the "anders" radio button is found
+    if (andersRadio) {
+        // Functie om zichtbaarheid te toggelen van andere opties
+        function toggleOtherFields() {
+            if (andersRadio.checked) {
+                console.log("Anders geselecteerd");
+                otherFields.forEach(field => field.style.display = 'block');
+            } else {
+                console.log("Bedrag optie geselecteerd");
+                otherFields.forEach(field => field.style.display = 'none');
+            }
         }
+
+        // Add event listener to the radio buttons in the "donatie_bedrag" group
+        const donationRadios = document.querySelectorAll('input[name="donatie_bedrag"]');
+        donationRadios.forEach(radio => {
+            radio.addEventListener('change', toggleOtherFields);
+        });
+
+        // Initial check in case "Anders" is pre-selected
+        toggleOtherFields();
     }
 
-    // Add event listener to the radio buttons in the "donatie_bedrag" group
-    const donationRadios = document.querySelectorAll('input[name="donatie_bedrag"]');
-    donationRadios.forEach(radio => {
-        radio.addEventListener('change', toggleOtherFields);
+    // <DETAILS>
+    // Het was mij niet gelukt om een "gecombineerde animatie" te maken met CSS en JS.
+    // Deze code heb ik met behulp van ChatGPT gemaakt.
+    document.querySelectorAll("details").forEach((detail) => {
+        detail.addEventListener("toggle", function () {
+            if (this.open) {
+                document.querySelectorAll("details").forEach((otherDetail) => {
+                    if (otherDetail !== this) {
+                        otherDetail.open = false;
+                    }
+                });
+            }
+        });
     });
-
-    // Initial check in case "Anders" is pre-selected
-    toggleOtherFields();
 });
-
-// HAMBURGER MENU
-var openButton = document.querySelector("header > button");
-
-openButton.onclick = openMenu;
-
-function openMenu() {
-    var deNav = document.querySelector("nav");
-    deNav.classList.add("toonMenu");
-}
-
-var sluitButton = document.querySelector("nav button");
-sluitButton.onclick = sluitMenu;
-
-function sluitMenu(){
-    var deNav = document.querySelector("nav");
-    deNav.classList.remove("toonMenu");
-}
-
-// HAMBURGER MENU
-var openButton = document.querySelector("header > button");
-
-openButton.onclick = openMenu;
-
-function openMenu() {
-    var deNav = document.querySelector("nav");
-    deNav.classList.add("toonMenu");
-}
-
-var sluitButton = document.querySelector("nav button");
-sluitButton.onclick = sluitMenu;
-
-function sluitMenu(){
-    var deNav = document.querySelector("nav");
-    deNav.classList.remove("toonMenu");
-}
